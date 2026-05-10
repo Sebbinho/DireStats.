b44227a7108b200f68ccf3cde1fd6ce200b333c0
Sebbinho <sebastiankreso@gmail.com>
2026-05-11 00:15:50 +0200

    Configurato Capacitor e GitHub Actions per APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.direstats.app',
-  appName: 'DireStats.',
+  appName: 'DireStats',
   webDir: 'dist'
 };
 
